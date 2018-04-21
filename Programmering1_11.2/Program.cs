07e675192e86326110bb77490992991941e1845d
Monica Björk <m.bjork95@gmail.com>
2018-04-21 10:59:41 +0200

    Add project files.

---
+++ Programmering1_11.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmering1_11._2 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Vad heter du? ");
+            string name = Console.ReadLine();
+            Console.Write("Vad är ditt favoritdjur? ");
+            string animal = Console.ReadLine();
+            Console.WriteLine(MyMethod(name, animal));
+            Console.ReadLine();
+        }
+
+        static string MyMethod(string name, string animal) {
+            string answer = "Hejsan " + name + "! Oj, " + animal + " är mitt favoritdjur också!";
+            return answer;
+        }
+    }
+}
